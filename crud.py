4f4cf1bbb98219a69a51bf962ae661cdc353f2d3
Vamshi Krishna Gujjari <vamshi@Vamshis-MacBook-Air.local>
2020-10-25 00:57:42 -0500

    Added the outline for all the tasks

--- crud.py
+++ crud.py
@@ -10,71 +10,118 @@ app = Flask(__name__)
 cred = credentials.Certificate('unilends.json')
 default_app = initialize_app(cred)
 db = firestore.client()
-todo_ref = db.collection('users')
+# todo_ref = db.collection('users')
+payments = db.collection('payments')
 
 
-@app.route('/add', methods=['POST'])
-def create():
+# @app.route('/add', methods=['POST'])
+# def create():
+#     """
+#         create() : Add document to Firestore collection with request body
+#         Ensure you pass a custom ID as part of json body in post request
+#         e.g. json={'id': '1', 'title': 'Write a blog post'}
+#     """
+#     try:
+#         id = request.json['id']
+#         payments.document(id).set(request.json)
+#         return jsonify({"success": True}), 200
+#     except Exception as e:
+#         return f"An Error Occured: {e}"
+
+
+# @app.route('/list', methods=['GET'])
+# def read():
+#     """
+#         read() : Fetches documents from Firestore collection as JSON
+#         todo : Return document that matches query ID
+#         all_todos : Return all documents
+#     """
+#     try:
+#         # Check if ID was passed to URL query
+#         todo_id = request.args.get('id')
+#         if todo_id:
+#             todo = todo_ref.document(todo_id).get()
+#             return jsonify(todo.to_dict()), 200
+#         else:
+#             all_todos = [doc.to_dict() for doc in todo_ref.stream()]
+#             return jsonify(all_todos), 200
+#     except Exception as e:
+#         return f"An Error Occured: {e}"
+
+
+# @app.route('/update', methods=['POST', 'PUT'])
+# def update():
+#     """
+#         update() : Update document in Firestore collection with request body
+#         Ensure you pass a custom ID as part of json body in post request
+#         e.g. json={'id': '1', 'title': 'Write a blog post today'}
+#     """
+#     try:
+#         id = request.json['id']
+#         todo_ref.document(id).update(request.json)
+#         return jsonify({"success": True}), 200
+#     except Exception as e:
+#         return f"An Error Occured: {e}"
+
+
+# @app.route('/delete', methods=['GET', 'DELETE'])
+# def delete():
+#     """
+#         delete() : Delete a document from Firestore collection
+#     """
+#     try:
+#         # Check for ID in URL query
+#         todo_id = request.args.get('id')
+#         todo_ref.document(todo_id).delete()
+#         return jsonify({"success": True}), 200
+#     except Exception as e:
+#         return f"An Error Occured: {e}"
+
+
+@app.route('/addPayment', methods=['POST'])
+def addPayments():
     """
-        create() : Add document to Firestore collection with request body
-        Ensure you pass a custom ID as part of json body in post request
-        e.g. json={'id': '1', 'title': 'Write a blog post'}
+    addPayment() : Add a payment to the feed, so that all the users can see it on their feed
     """
     try:
-        id = request.json['id']
-        todo_ref.document(id).set(request.json)
+        id = request.json['paymentId']
+        print(id)
+        payments.document(id).set(request.json)
         return jsonify({"success": True}), 200
     except Exception as e:
-        return f"An Error Occured: {e}"
+        return "An error occurred"
 
 
-@app.route('/list', methods=['GET'])
-def read():
+@app.route('/makePayment', methods=['POST'])
+def makePayments():
     """
-        read() : Fetches documents from Firestore collection as JSON
-        todo : Return document that matches query ID
-        all_todos : Return all documents
+    makePayment() : Making payment on a particular request. Request body contains the payment ID 
+    Response will be the success/failure of the payment
     """
     try:
-        # Check if ID was passed to URL query
-        todo_id = request.args.get('id')
-        if todo_id:
-            todo = todo_ref.document(todo_id).get()
-            return jsonify(todo.to_dict()), 200
+        payment_id = request.body['id']
+        if payment_id and payments.document(payment_id).get():
+            payments.document(payment_id).update({"paid":True})
+            return jsonify({"success": True}), 200
         else:
-            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
-            return jsonify(all_todos), 200
+            raise Exception("Payment Id not found") 
+
     except Exception as e:
-        return f"An Error Occured: {e}"
+        return "An error occurred" 
 
-
-@app.route('/update', methods=['POST', 'PUT'])
-def update():
+@app.route('/getAll', methods=['POST'])
+def getAll():
     """
-        update() : Update document in Firestore collection with request body
-        Ensure you pass a custom ID as part of json body in post request
-        e.g. json={'id': '1', 'title': 'Write a blog post today'}
+    getAll() : gets the feed for the current user
     """
     try:
-        id = request.json['id']
-        todo_ref.document(id).update(request.json)
+
         return jsonify({"success": True}), 200
     except Exception as e:
-        return f"An Error Occured: {e}"
+        return "An error occurred" 
 
 
-@app.route('/delete', methods=['GET', 'DELETE'])
-def delete():
-    """
-        delete() : Delete a document from Firestore collection
-    """
-    try:
-        # Check for ID in URL query
-        todo_id = request.args.get('id')
-        todo_ref.document(todo_id).delete()
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
+
 
 
 port = int(os.environ.get('PORT', 8080))
